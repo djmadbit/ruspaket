373df42bbcccd99ab280a95de95730e3aabbaebf
root <root@d8.best-hosting.ru>
2017-12-22 08:22:01 -0500

    Добавил автозаполнение utm метки из название.

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,11 @@ from .models import Category,Item,Order
 
 class CategoryAdmin(admin.ModelAdmin):
 	list_display=['title','price_from','price_to','min_count']
+	prepopulated_fields = {'utm': ('title',)}
 
 class ItemAdmin(admin.ModelAdmin):
 	list_display=['title','utm','price_from','price_to','min_count','category']
+	prepopulated_fields = {'utm': ('title',)}
 
 class OrderAdmin(admin.ModelAdmin):
 	list_display=['name','phone','desc','date_created']
